3eb0d779c6f2097f280512e2a2814dd52c4b58c7
Benjamin Lin <blin432@gmail.com>
2019-07-04 12:38:05 -0400

    calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,30 @@
-function add () {
-	
-}
-
-function subtract () {
-	
-}
-
-function sum () {
-	
-}
-
-function multiply () {
-	
-}
-
-function power() {
-	
-}
-
-function factorial() {
-	
-}
-
-module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+function add(a, b) {
+	return a + b;
+  }
+  
+  function subtract(a, b) {
+	return a - b;
+  }
+  
+  function sum(array) {
+	return array.reduce((current, total) => total + current, 0);
+  }
+  
+  function multiply(array) {
+	  return array.length
+		? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+		: 0;
+  }
+  
+  function power(a, b) {
+	return Math.pow(a, b);
+  }
+  
+  function factorial(n) {
+	if (n == 0) return 1;
+	let product = 1;
+	for (let i = n; i > 0; i--) {
+	  product *= i;
+	}
+	return product;
+  }
