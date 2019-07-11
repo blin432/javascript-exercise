49a0582433c2b7658bf4e0ce53c654e29bb7174d
Benjamin Lin <blin432@gmail.com>
2019-07-10 21:59:47 -0400

    practice

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
 var getTheTitles = function(array) {
-    return array.map(book => book.title)
-  }
+   return array.map(book=> book.title)
+   }
+  
   
   module.exports = getTheTitles
--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
-}
-
-module.exports = repeatString
+var repeatString = function(word, times) {
+    let string = '';
+    for (var i = 0; i<times; i++){
+        string+=word;
+    }
+    return string;
+  }
+  
+  module.exports = repeatString
